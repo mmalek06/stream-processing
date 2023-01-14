7af3aadaac331c7bd35d697f967a8287a77aa2ed
Marek Małek <mmalek06@gmail.com>
2023-01-14 19:32:51 +0100

    initial data parsing

--- src/StreamProcessing.Server/StreamProcessing.DataSender/Program.cs
+++ src/StreamProcessing.Server/StreamProcessing.DataSender/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Globalization;
+using System.Text;
 using StreamProcessing.Server.Resources;
 
 Console.WriteLine(@"Running data sender - this will simulate the process of sending data belonging to one wind turbine.");
@@ -12,7 +13,23 @@ const int InformEveryNLines = 1000;
 
 foreach (var line in lines)
 {
-    
+    // ignore the header
+    if (counter == 0)
+    {
+        counter++;
+        
+        continue;
+    }
+
+    var parts = line
+        .Split(',')
+        .Select(x => x.Trim())
+        .ToArray();
+    var date = DateTime.ParseExact(parts[0], "dd MM yyyy HH:mm", CultureInfo.InvariantCulture);
+    var activePowerLevel = float.Parse(parts[1]);
+    var msWindSpeed = float.Parse(parts[2]);
+    var theoreticalPowerCurve = float.Parse(parts[3]);
+    var windDirection = float.Parse(parts[4]);
     
     counter++;
     
